83059060ab25ba0eefbf941ca0b8f007ee95d429
Gábor Librecz <kuglee@gmail.com>
2024-09-01 16:25:13 +0200

    Us a generic list reducer

--- TCACasePathIssue/ContentView.swift
+++ TCACasePathIssue/ContentView.swift
@@ -5,14 +5,14 @@ import SwiftUI
   public init() {}
 
   @ObservableState public struct State: Equatable {
-    public var listState: RemoteResult<ListReducer.State, AppError>
+    public var listState: RemoteResult<ListReducer<ChildFeature>.State, AppError>
 
-    public init(listState: RemoteResult<ListReducer.State, AppError> = .initial) {
+    public init(listState: RemoteResult<ListReducer<ChildFeature>.State, AppError> = .initial) {
       self.listState = listState
     }
   }
 
-  public enum Action: Sendable { case listAction(ListReducer.Action) }
+  public enum Action: Sendable { case listAction(ListReducer<ChildFeature>.Action) }
 
   public var body: some ReducerOf<Self> {
     Reduce { state, action in
@@ -22,29 +22,6 @@ import SwiftUI
     }
     .ifLet(\.listState.success, action: \.listAction) { ListReducer() }
   }
-
-  @Reducer public struct ListReducer: Sendable {
-    public init() {}
-
-    @ObservableState public struct State: Equatable {
-      public var childStates: IdentifiedArrayOf<ChildFeature.State>
-
-      public init(childStates: IdentifiedArrayOf<ChildFeature.State> = []) {
-        self.childStates = childStates
-      }
-    }
-
-    public enum Action: Sendable { case childAction(IdentifiedActionOf<ChildFeature>) }
-
-    public var body: some ReducerOf<Self> {
-      Reduce { state, action in
-        switch action {
-        case .childAction: return .none
-        }
-      }
-      .forEach(\.childStates, action: \.childAction) { ChildFeature() }
-    }
-  }
 }
 
 public struct AppView: View {
@@ -53,16 +30,59 @@ public struct AppView: View {
   public init(store: StoreOf<AppFeature>) { self.store = store }
 
   public var body: some View {
-    if let store = self.store.scope(state: \.listState.success, action: \.listAction) {
-      ForEach(store.scope(state: \.childStates, action: \.childAction)) { ChildView(store: $0) }
+    switch self.store.listState {
+    case .success:
+      if let store = self.store.scope(state: \.listState.success, action: \.listAction) {
+        ForEach(store.scope(state: \.childStates, action: \.childAction)) { ChildView(store: $0) }
+      }
+    default: EmptyView()
     }
   }
 }
 
-@Reducer public struct ChildFeature: Sendable {
+@Reducer public struct ListReducer<ChildFeature: Reducer & DefaultInit>: Sendable
+where
+  ChildFeature.State: Equatable & Identifiable & Sendable,
+  ChildFeature.State.ID: Sendable,
+  ChildFeature.Action: Sendable
+{
   public init() {}
 
-  @ObservableState public struct State: Equatable, Identifiable {
+  @dynamicMemberLookup @ObservableState public struct State: Equatable, Sendable {
+    public var childStates: IdentifiedArrayOf<ChildFeature.State>
+
+    public init(childStates: IdentifiedArrayOf<ChildFeature.State> = []) {
+      self.childStates = childStates
+    }
+
+    public subscript<T>(dynamicMember keyPath: KeyPath<IdentifiedArrayOf<ChildFeature.State>, T>)
+      -> T
+    { self.childStates[keyPath: keyPath] }
+
+    public subscript<T>(
+      dynamicMember keyPath: WritableKeyPath<IdentifiedArrayOf<ChildFeature.State>, T>
+    ) -> T {
+      get { self.childStates[keyPath: keyPath] }
+      set { self.childStates[keyPath: keyPath] = newValue }
+    }
+  }
+
+  public enum Action: Sendable { case childAction(IdentifiedActionOf<ChildFeature>) }
+
+  public var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .childAction: return .none
+      }
+    }
+    .forEach(\.childStates, action: \.childAction) { ChildFeature() }
+  }
+}
+
+@Reducer public struct ChildFeature: Sendable, DefaultInit {
+  public init() {}
+
+  @ObservableState public struct State: Equatable, Identifiable, Sendable {
     public let id: UUID
 
     public init(id: UUID = UUID()) { self.id = id }
@@ -88,3 +108,5 @@ public struct ChildView: View {
 }
 
 public enum AppError: Equatable, Codable, Sendable, Error, LocalizedError { case unauthorized }
+
+public protocol DefaultInit { init() }
